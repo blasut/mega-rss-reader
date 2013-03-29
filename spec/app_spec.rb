11ed01d5645459fbadd4b975ee302f1be7902ac0
Christopher Schmölzer <christopher.schmolzer@gmail.com>
2013-03-29 21:19:33 +0100

    add app spec

---
+++ spec/app_spec.rb
@@ -0,0 +1,15 @@
+describe App do
+  it "should show number of unread article"
+
+  it "should show a list of all unread articles"
+
+  it "should move to the next item when 'J' is pressed"
+
+  it "should move to the previous item when 'K' is pressed"
+
+  it "should open an excerpt when 'O' is pressed"
+
+  it "should save an article to readability when 'R' is pressed"
+
+  it "should open the article in the browser when 'B' is pressed"
+end
